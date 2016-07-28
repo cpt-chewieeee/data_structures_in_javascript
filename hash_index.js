2cc2de36b1d1a66828dd8b804126947599f48f6d
Hongyi Zhang <hzhan014@gmail.com>
2016-07-27 19:25:16 -0700

    adding mac side stuff

--- hash_index.js
+++ hash_index.js
@@ -1,8 +1,21 @@
 'use strict';
-var Reader = require('./Reader.js');
-var reader = new Reader();
+// var Reader = require('./Reader.js');
+// var reader = new Reader();
 
-var file = './data6_17.json';
+// var file = './data6_17.json';
 
 // reader.createDb();
 // reader.readLine(file);
+
+var BST = require('./BST');
+var bst = new BST();
+
+
+var test = [3, 4, 1, 5, 7];
+for(var i = 0; i < test.length; ++i){
+	bst.insert(test[i]);
+}
+
+console.log(bst.contains(1));
+console.log(bst.contains(2));
+console.log(bst.contains(9));
